8c7be5737b7f7e9f416825783e6de344aa24aa41
Pedro Ferreira <plferreira4@gmail.com>
2011-07-19 23:17:56 +0100

    adding the license

--- main.cpp
+++ main.cpp
@@ -1,3 +1,25 @@
+/*
+
+	Copyright - 2011 - Pedro Ferreira
+
+	This file is part of metronome_controller.
+
+    metronome_controller is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    metronome_controller is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with metronome_controller.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+
 //avrdude -p m328p -c arduino -P /dev/ttyACM0 -U flash:w:metronome_controller.elf.hex
 
 #include <avr/interrupt.h>
